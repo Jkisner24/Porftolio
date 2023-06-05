48b0abbaf85867cf331cfbd6ee825e481d31db75
Julian Kisner <julikisner@gmail.com>
2023-06-05 12:06:30 -0300

    add

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-const User = require('./models/user')
+const User = require('./api/models/user')
 
 require('dotenv').config();
 
